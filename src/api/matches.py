b9ccf6c115c3ebf548678a7bafa51b07f93cdf07
David Montiel <davidmontiel549@gmail.com>
2026-05-05 12:54:44 -0700

    feat: API responses

--- src/api/matches.py
+++ src/api/matches.py
@@ -24,54 +24,64 @@ class Particapant(BaseModel):
     team: int
 
 
+class Response(BaseModel):
+    success: bool
+    msg: str
+
+
 @router.post("/", status_code=status.HTTP_200_OK)
 def post_match(match: Match, players: List[Particapant]):
-    with db.engine.begin() as conn:
-        match_id = conn.execute(
-            sa.text(
-                """
-                    INSERT INTO matches (score)
-                    VALUES (:score)
-                    RETURNING id
+    try:
+        with db.engine.begin() as conn:
+            match_id = conn.execute(
+                sa.text(
                     """
-            ),
-            {"score": match.score},
-        ).scalar_one()
+                        INSERT INTO matches (score)
+                        VALUES (:score)
+                        RETURNING id
+                        """
+                ),
+                {"score": match.score},
+            ).scalar_one()
 
-        players_list = [
-            {
-                "player_id": player.player_id,
-                "match_id": match_id,
-                "winner": player.winner,
-                "team": player.team,
-            }
-            for player in players
-        ]
+            players_list = [
+                {
+                    "player_id": player.player_id,
+                    "match_id": match_id,
+                    "winner": player.winner,
+                    "team": player.team,
+                }
+                for player in players
+            ]
 
-        conn.execute(
-            sa.text(
-                """
-                INSERT INTO match_participants (player_id, match_id, winner, team)
-                VALUES (:player_id, :match_id, :winner, :team)
-                """
-            ),
-            players_list,
-        )
-
-    return
+            conn.execute(
+                sa.text(
+                    """
+                    INSERT INTO match_participants (player_id, match_id, winner, team)
+                    VALUES (:player_id, :match_id, :winner, :team)
+                    """
+                ),
+                players_list,
+            )
+    except sa.exc.SQLAlchemyError:
+        return Response(success=False, msg="Internal Server Error")
+    return Response(success=True, msg=f"match id: {match_id}")
 
 
 @router.put("/{match_id}", status_code=status.HTTP_204_NO_CONTENT)
 def update_match(match_id: int, score: Match):
-    with db.engine.begin() as conn:
-        conn.execute(
-            sa.text(
-                """
-                    UPDATE matches
-                    SET 
-                    score = :score
-                    WHERE id = :match_id
+    try:
+        with db.engine.begin() as conn:
+            conn.execute(
+                sa.text(
                     """
-            ),
-            {"score": score.score, "match_id": match_id},
-        )
+                        UPDATE matches
+                        SET 
+                        score = :score
+                        WHERE id = :match_id
+                        """
+                ),
+                {"score": score.score, "match_id": match_id},
+            )
+    except sa.exc.SQLAlchemyError:
+        raise status.HTTP_500_INTERNAL_SERVER_ERROR
--- src/api/player.py
+++ src/api/player.py
@@ -26,55 +26,65 @@ class PlayerStatistics(BaseModel):
 
 class PlayersResponse(BaseModel):
     success: bool
+    msg: str
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
 def post_player(player: Player, stats: PlayerStatistics):
-    with db.engine.begin() as conn:
-        player_id = conn.execute(
-            sa.text(
-                """
-                    INSERT INTO players (name, utr_rating)
-                    VALUES (:name, :utr)
-                    RETURNING id
+    try:
+        with db.engine.begin() as conn:
+            player_id = conn.execute(
+                sa.text(
                     """
-            ),
-            {"name": player.name, "utr": player.utr},
-        ).scalar_one()
+                        INSERT INTO players (name, utr_rating)
+                        VALUES (:name, :utr)
+                        RETURNING id
+                        """
+                ),
+                {"name": player.name, "utr": player.utr},
+            ).scalar_one()
 
-        conn.execute(
-            sa.text(
+            conn.execute(
+                sa.text(
+                    """
+                    INSERT INTO  player_stats (player_id, first_serve_percentage, second_serve_percentage, matches_won)
+                    VALUES (:id, :first, :second, :won)
                 """
-                INSERT INTO  player_stats (player_id, first_serve_percentage, second_serve_percentage, matches_won)
-                VALUES (:id, :first, :second, :won)
-            """
-            ),
-            {
-                "id": player_id,
-                "first": stats.first_serve_percentage,
-                "second": stats.second_serve_percentage,
-                "won": stats.matches_won,
-            },
-        )
+                ),
+                {
+                    "id": player_id,
+                    "first": stats.first_serve_percentage,
+                    "second": stats.second_serve_percentage,
+                    "won": stats.matches_won,
+                },
+            )
+    except sa.exc.SQLAlchemyError:
+        return PlayersResponse(success=False, msg="Internal Server Error")
+    return PlayersResponse(success=True, msg=f"Player id: {player_id}")
 
 
 @router.put("/{player_id}", status_code=status.HTTP_204_NO_CONTENT)
 def update_player(statistics: PlayerStatistics, player_id: int):
-    with db.engine.begin() as conn:
-        conn.execute(
-            sa.text(
-                """
-                    UPDATE player_stats
-                    SET
-                        first_serve_percentage = :first,
-                        second_server_percentage = :second,
-                        matches_won = :won
-                    WHERE id = :id
+    try:
+        with db.engine.begin() as conn:
+            conn.execute(
+                sa.text(
                     """
-            ),
-            {
-                "first": statistics.first_serve_percentage,
-                "second": statistics.second_serve_percentage,
-                "won": statistics.matches_won,
-            },
-        )
+                        UPDATE player_stats
+                        SET
+                            first_serve_percentage = :first,
+                            second_server_percentage = :second,
+                            matches_won = :won
+                        WHERE id = :id
+                        """
+                ),
+                {
+                    "first": statistics.first_serve_percentage,
+                    "second": statistics.second_serve_percentage,
+                    "won": statistics.matches_won,
+                },
+            )
+
+    except sa.exc.SQLAlchemyError:
+        return PlayersResponse(success=False, msg="Internal Server Error")
+    return PlayersResponse(success=True, msg="")
